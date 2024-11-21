da374692ea03f34bec53041f0d2639ec1d85e39f
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 13:48:17 +0000

    Correction

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("{}".format(''.join(chr(i) for i in range(97, 123) if chr(i) != 'e' or chr(i) != 'q')), end="")
+print("{}".format(''.join(chr(i) for i in range(97, 123) if chr(i) not in 'qe')), end="")
