9a2d031f62a9548914b7d38247162e89bf77678b
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 16:36:00 +0000

    exo 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    up = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            up += chr(ord(i)-32)
+        else:
+            up += i
+    print("{}".format(up))
+        
