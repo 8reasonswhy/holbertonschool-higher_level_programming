ab52768a44e06154f413365d448b2afab9868ecc
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 14:34:07 +0000

    exo 4

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-nm = "/n".join([f"{i} = {hex(i)}" for i in range(99)]
-print("{}".format(rm), end="")
+nm = "\n".join([f"{i} = {hex(i)}" for i in range(99)])
+print("{}".format(nm), end="")
