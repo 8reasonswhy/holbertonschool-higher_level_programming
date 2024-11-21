37b2c889675a49391f48bb5f90ac8846ee55f22a
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 12:53:34 +0000

    les espaces

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,4 +7,4 @@ if last_digit == 0:
 elif last_digit > 5:
     print("Last digit of", number, "is", last_digit, "and is geater then 5")
 else:
-     print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+     print("Last digit of", number, "is", last_digit, "and is less then 6 and not 0")
