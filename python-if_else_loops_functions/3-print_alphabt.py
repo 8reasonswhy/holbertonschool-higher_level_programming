3d5aa2558df796d35cf81a8eae2a93dfdfcead41
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 14:11:09 +0000

    what ever

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-alphabet = ''.join(chr(i) for i in range(97, 123) if chr(i) != 'q' and chr(i) != 'e')
-print("{}".format(alphabet), end="")
+a_t = ''.join(chr(i) for i in range(97, 123) if chr(i) != 'q' and chr(i) != 'e')
+print("{}".format(a_t), end="")
