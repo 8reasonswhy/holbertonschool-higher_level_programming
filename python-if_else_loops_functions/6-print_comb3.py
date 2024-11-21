89e6c735cf28cc96b52be0a85416f1c06fa7659a
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 16:00:58 +0000

    Correction de l'exo 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-num = ", ".join([f"{i:02}" for i in range(1, 99) if int(str(i)[::-1]) > i])
+num = ", ".join([f"{i:02}" for i in range(1, 99) if int(f"{i:02}"[::-1]) > i])
 print("{}".format(num))
