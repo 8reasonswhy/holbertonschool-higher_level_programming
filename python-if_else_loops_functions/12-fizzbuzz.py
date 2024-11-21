b8dfbe363a4ac87349cbc57ebd8dd726e73b6eff
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 17:19:24 +0000

    exo 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    rt=""
+    for i in range (101):
+        if i % 3 == 0:
+            rt += "Fizz"
+            if i % 5 == 0:
+                rt += "Buzz "
+            else:
+                rt += " "
+        elif i % 5 ==0:
+            rt += "Buzz "
+        else:
+            rt += str(i)+" "
+
+
+
