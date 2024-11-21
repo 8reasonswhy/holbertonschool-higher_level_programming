9ad4cd05a356ecafee307ce1c15e05da763f5f93
8reasonswhy <yass.candidature@gmail.com>
2024-11-21 17:27:30 +0000

    exo 12 corr

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     rt = ""
-    for i in range (1, 101):
+    for i in range(1, 101):
         if i % 3 == 0:
             rt += "Fizz"
             if i % 5 == 0:
