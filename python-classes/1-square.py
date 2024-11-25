df1de78d9d4a929847daa534f06a84c29d64073d
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 19:17:37 +0000

    faut pas utiliser e accent en comm

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,12 +6,12 @@ Class squar avec un privat attribute size
 
 class Square :
     """
-    class Squar avec un attribue size private un attribue prive on lui ajout un double  _ devant lui pour dir que c'est priv√
+    class Squar avec un attribue size private un attribue prive on lui ajout un double  _ devant lui pour dir que c est priv√
     """
 
 
     def __init__(self,size):
-        """ Alors la on d√©fini le constructeur appeler init sur python le constructeur est appele automatiquement 
+        """ Alors la on de©fini le constructeur appeler init sur python le constructeur est appele automatiquement 
         quand on veut instancier un nouveau object 
         """
         self.__size = size
