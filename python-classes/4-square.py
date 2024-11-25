c6d33f775ab2b1211510c3a6da8cca6d143eee63
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 21:08:13 +0000

    Les tage @property et @size.setter sont obligatoir

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,33 +9,37 @@ class Square:
     """
     Class representing a square with a private size attribute.
     """
+
     def __init__(self, size=0):
         """
         The __init__ method is a constructor that is called automatically
         when a new object is instantiated.
+        Qaund on veut initialiser le truc pythin fait appel automatique au setter grace au tag
         """
-        if not isinstance(size, int):
+        self.size = size  # Utilise le setter pour initialiser
+
+    @property
+    def size(self):
+        """
+        Getter pour récupérer  valeur de size car elle est prive 
+        on est obliger d utiliser le tag  @property
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter pour modifier la valeur de notre trucs privee il faut obligatoirement 
+        utiliser le tag @<nom de votre truc>.setter
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = value
 
     def area(self):
         """
-        This method returns the area of the square (size squared).
+        Méthode pour calculer l'aire du carré (taille au carré).
         """
         return self.__size ** 2
-    
-    def size(self, value):
-        """
-        la definition d un setter car l attribu est privee
-        """
-        self.__size = value
-   
-   def size(self):
-       """
-       on defini un getter car le truc est privee et pour acceder il nous faut un setter
-       """
-       return self.__size
-~                                  
