2c67e65ceee888cd14a6c2d4c6c9bd3b9b530d54
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 20:32:27 +0000

    Creation d'une Getter

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module defines a class Square to represent a square shape.
+"""
+
+
+class Square:
+    """
+    Class square on defini la class avec le mot class 
+    """
+    def __init__(self, size=0):
+        """
+        la methode init est un constructeur appele automatiquement lors de 
+        l instanciacion d un nouveau object 
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        def area(self):
+            return self.__size**2
