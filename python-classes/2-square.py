b1bb15154f9932e5a4d4924ecb070cb136c13e29
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 20:17:28 +0000

    bref les espace etc..

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,18 +4,19 @@
 This module defines a class Square to represent a square shape.
 """
 
+
 class Square:
     """
     A class that defines a square
     """
     def __init__(self, size=0):
         """
-        La méthode __init__ est un constructeur qui est appelé automatiquement lors de 
+        La méthode __init__ est un constructeur qui est appeé automatiquementfor 
         l'instanciation d'un nouvel objet
         """
         if not isinstance(size, int):
-            raise TypeError ('size must be an integer')
-        elif size < 0 :
-            raise ValueError ("size must be >= 0")
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
