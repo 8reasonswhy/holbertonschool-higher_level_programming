5d960150a4ab1fcfef36c1142a703d70651c4ae8
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 19:33:46 +0000

    ABUSEEEE FRERE

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,7 +8,6 @@ This module defines a class Square to represent a square shape.
 class Square:
     """
     A class that defines a square.
-
     This class does not currently implement any attributes or methods,
     but it is a placeholder for potential future features related to squares.
     """
@@ -16,6 +15,6 @@ class Square:
     def __init__(self, s):
         """
          __init_ un constructeur qui est appelé automatiquemen pour
-        l'instanciation 
+        l'instanciation
         """
         self.__size = s
