a3ff53b83d3e63def02c843265806ec6a8cb2150
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 18:23:48 +0000

    ajout des doc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-class Squar :
-    pass
+"""
+This module defines a class Square to represent a square.
+"""
 
+class Square:
+    """
+    A class that defines a square.
+    """
+    pass
