618c582c717d9369ce0b5fd1fb89f1e3f373754d
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 21:34:03 +0000

    On veut imprimer le carrer sous forme de #

---
+++ python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module defines a class Square to represent a square shape.
+"""
+
+
+class Square:
+    """
+    Class representing a square with a private size attribute.
+    """
+
+    def __init__(self, size=0):
+        """
+        The __init__ method is a constructor that is called automatically
+        when a new object is instantiated.
+        Qaund on veut initialiser le truc python
+        fait appel automatique au setter grace au tag
+        """
+        self.size = size  # Utilise le setter pour initialiser
+
+    @property
+    def size(self):
+        """
+        Getter pour récupérer  valeur de size car elle est pri
+        on est obliger d utiliser le tag  @property
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter pour modifier la valeur de notre trucs privee il faut
+        obligatoirement utiliser le tag @<nom de votre truc>.setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Méthode pour calculer l'aire du care
+        """
+        return self.__size ** 2
+    
+    def my_print(self):
+        """
+        la pour imprimer une image de notre carree on va dire
+        """
+        if self.size == 0:
+            print('')
+        else:
+            for i in range(self.size):
+                print('#'*self.size)
+
