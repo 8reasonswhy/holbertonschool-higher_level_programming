ba122a17e5c3246262c3c2a4ade25ec017998bd6
8reasonswhy <yass.candidature@gmail.com>
2024-11-25 18:27:08 +0000

    n import

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-This module defines a class Square to represent a square.
+This module defines a class Square to represent a square shape.
 """
 
 class Square:
     """
     A class that defines a square.
+
+    This class does not currently implement any attributes or methods,
+    but it is a placeholder for potential future features related to squares.
     """
     pass
