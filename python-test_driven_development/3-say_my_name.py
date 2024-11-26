0a775b7a9979e6bb6a7968bee8e39b226e451238
8reasonswhy <yass.candidature@gmail.com>
2024-11-26 13:04:29 +0000

    corriger les testes

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,7 +6,7 @@ def say_my_name(first_name, last_name=""):
     """
     la fonction qui fait le trucs 
     """
-    if not(isinstance(first_name, str)):
+    if not(isinstance(first_name, str)or first_name is None):
         raise TypeError("first_name must be a string")
     elif not(isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
