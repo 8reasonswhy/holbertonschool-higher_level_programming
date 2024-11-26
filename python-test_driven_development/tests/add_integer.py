5bd99914242f4f1844d7818ef116ed44ba84f0ae
8reasonswhy <yass.candidature@gmail.com>
2024-11-26 12:12:19 +0000

    les test nous hebel

---
+++ python-test_driven_development/tests/add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Le programme qui fais les teste et nous fait l' addition des trucs 
+"""
+def add_integer(a, b):
+    """
+    notre fonction qui fait l'ajout de deux entier
+    """
+    if not (isinstance(a, (int, float)) or a is None):
+        raise TypeError("a must be an integer")
+    if not ( isinstance(b, (int, float)) or b is None):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
