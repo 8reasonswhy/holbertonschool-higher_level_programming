956665363bd6f2f65c1f40395e4ac9ae5caa73ac
8reasonswhy <yass.candidature@gmail.com>
2024-11-26 11:29:28 +0000

    faut faire plus de truc afin de valider les teste

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""
+Le programme qui fais les teste et nous fait l' addition des trucs 
+"""
 def add_integer(a, b):
     """
     notre fonction qui fait l'ajout de deux entier
     """
-    if not (isinstance(a, (int, float))):
+    if not (isinstance(a, (int, float)) and a is None):
         raise TypeError("a must be an integer")
-    if not ( isinstance(b, (int, float))):
+    else:
+        a = int(a)
+    if not ( isinstance(b, (int, float)) and b is None):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    else:
+        b = int(b)
+    return a + b
 
