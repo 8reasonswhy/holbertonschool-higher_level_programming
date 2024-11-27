b2d91ca5f9f6ea72803022f8366ef87e57cee542
8reasonswhy <yass.candidature@gmail.com>
2024-11-27 14:43:52 +0000

    les espace etcc..

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,8 +8,8 @@ def add_integer(a, b):
     """
     notre fonction qui fait l'ajout de deux entier
     """
-    if not (isinstance(a, (int, float)) or a is None):
+    if not(isinstance(a, (int, float)) or a is None):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, (int, float)) or b is None):
+    if not(isinstance(b, (int, float)) or b is None):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
