13e91d25d43abd7ee3b569ed27e750fc6bd49c2b
8reasonswhy <yass.candidature@gmail.com>
2024-11-27 15:06:46 +0000

    exo 4 les espace etcc

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,13 +2,15 @@
 """
 la fonction qui dessice le truc
 """
+
+
 def print_square(size):
     """
     la fonction qui fait les trucs
     """
-    if not(isinstance(size, int)):
+    if not (isinstance(size, int)):
         raise TypeError("size must be an integer")
-    elif size < 0 :
+    elif size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
         print("#"*size)
